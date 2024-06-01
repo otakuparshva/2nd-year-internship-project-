22bf6bd92da803190a7e6be5f6daaa4f00c9acb2
KaranTheGod13 <129877799+KaranTheGod13@users.noreply.github.com>
2024-06-01 16:36:11 +0530

    #18 feature(wip): home page

---
+++ one/home/templates/home/script.js
@@ -0,0 +1,7 @@
+
+    $(document).ready(function() {
+        $('#play-button').click(function() {
+            // Handle the "Play Now" button click event
+            console.log("Play Now button clicked!");
+        });
+    });
