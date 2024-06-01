ed454ca3b5e165a09d9862e6ef6b37b58655d34d
KaranTheGod13 <129877799+KaranTheGod13@users.noreply.github.com>
2024-06-01 17:06:49 +0530

    #18 some changes have been made

--- one/home/templates/home/script.js
+++ one/home/templates/home/script.js
@@ -1,7 +1,20 @@
-
-    $(document).ready(function() {
-        $('#play-button').click(function() {
-            // Handle the "Play Now" button click event
-            console.log("Play Now button clicked!");
-        });
+$(document).ready(function() {
+    $('#play-button').click(function() {
+        // Handle the "Play Now" button click event
+        console.log("Play Now button clicked!");
+        alert("Let's play the game!");
     });
+
+    // Additional animations or interactivity can be added here
+    $('nav a').hover(function() {
+        $(this).addClass('animate-pulse');
+    }, function() {
+        $(this).removeClass('animate-pulse');
+    });
+});
+/* JavaScript */
+
+// Button Pulse
+setInterval(function() {
+    $('#play-button').toggleClass('pulse');
+}, 1000);
